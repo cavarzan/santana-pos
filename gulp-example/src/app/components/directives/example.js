5d01d49f2086ad4cc55a6cc8eaa50d02b0fa9c5d
Deividi Alexandre Cavarzan <deividi.cavarzan@gmail.com>
2017-10-07 09:07:31 -0300

    directive

--- gulp-example/src/app/components/directives/example.js
+++ gulp-example/src/app/components/directives/example.js
@@ -1,6 +1,6 @@
 angular
   .module('explorer')
-  .directive("namePrinter", function() {
+  .directive("namePrinterTest", function() {
     return {
       restrict: 'EA',
       templateUrl : 'app/components/directives/example.html',
@@ -10,7 +10,17 @@ angular
         and : '&'
       },
       link : function (scope, element, attributes) {
-          console.log(scope);
+          console.log(scope.eq);
+          console.log(scope.$eval(scope.at));
+          console.log(scope.and());
+          scope.fieldText = "Meu campo"
+          scope.$watch('fieldText', function (n, o) {
+            if (n === 'BATMAN') {
+              element.css({
+                'color' : 'red'
+              });
+            }
+          });
       },
       // controller: NamePrinterController,
       // controllerAs: 'vm',
